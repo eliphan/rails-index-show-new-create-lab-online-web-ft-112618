0671297c70dd974e2cd8f4ce9ee138d8d1e41032
Elise Phan <aliceconnic@gmail.com>
2019-02-06 00:54:31 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,7 +9,7 @@ class CouponsController < ApplicationController
   end
   
   def create
-    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: [:store])
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: [:coupon][:store])
     redirect_to coupon_path(@coupon)
   end
   
